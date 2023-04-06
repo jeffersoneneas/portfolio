270374f149e24c9e590841619337c8ec11f47a6c
Jefferson Eneas da Silva <jeffersoneneassilva@gmail.com>
2023-04-06 17:34:57 -0300

    botao-para-ir-ao-topo

--- main.js
+++ main.js
@@ -15,3 +15,28 @@ linksInternos.forEach(link => {
         });
     });
 });
+
+// seleciona o botão de voltar ao topo
+const btnTopo = document.querySelector('#btn-topo');
+
+// adiciona um evento de clique ao botão
+btnTopo.addEventListener('click', function () {
+    // faz a rolagem suave até o topo da página
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+// adiciona um evento de rolagem à página
+window.addEventListener('scroll', function () {
+    // verifica a posição atual da página
+    const posicaoAtual = window.pageYOffset;
+
+    // se a posição atual for maior do que 500px, exibe o botão
+    if (posicaoAtual > 500) {
+        btnTopo.style.display = 'block';
+    } else {
+        btnTopo.style.display = 'none';
+    }
+});
